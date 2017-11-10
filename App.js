f57b72dde92d63ca8835a60be464bc5ea0c5c092
youngjoo <urdlove@gmail.com>
2017-11-10 16:13:51 +0900

    Init project

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Platform, WebView, View, StatusBar, BackHandler } from 'react-native';
+import WebViewAndroid from 'react-native-webview-android';
+
+export default class App extends Component {
+  componentDidMount = () => {
+    BackHandler.addEventListener('hardwareBackPress', this.backHandler);
+  };
+
+  componentWillUnmount = () => {
+    BackHandler.removeEventListener('hardwareBackPress', this.backHandler);
+  };
+
+  backHandler = () => {
+    const now = new Date();
+    // double press on android back button
+    if (now.getTime() - this.state.backPressTime < 250) {
+      return false;
+    }
+
+    this.setState(prevState => ({ backPressTime: now.getTime() }));
+
+    if (true) {
+      this.webview.goBack();
+      return true;
+    }
+  };
+
+  render() {
+    const url = 'http://192.168.10.53:3000';
+    // const url = 'http://van.aty.kr';
+
+    if (Platform.OS === 'ios') {
+      return (
+        <View style={{ flex: 1 }}>
+          <StatusBar backgroundColor="#fe931f" barStyle="light-content" hidden />
+          <WebView
+            source={{ uri: url }}
+            startInLoadingState
+            scalesPageToFit
+            javaScriptEnabled
+            bounces={false}
+            style={{ flex: 1 }}
+          />
+        </View>
+      );
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <WebViewAndroid
+          ref={webview => (this.webview = webview)}
+          javaScriptEnabled
+          url={url}
+          style={{ flex: 1 }}
+        />
+      </View>
+    );
+  }
+}
